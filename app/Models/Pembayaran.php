013a38d3b0f295030bb453b0f7391bafe765844a
patikabel <patikaabel@gmail.com>
2023-03-22 21:23:54 +0700

    migrasi dan model

---
+++ app/Models/Pembayaran.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pembayaran extends Model
+{
+    use HasFactory;
+
+    protected $table = 'pembayarans';
+
+    protected $primaryKey = 'id_pembayaran';
+
+    protected $guarded = ['id_pembayaran'];
+}
